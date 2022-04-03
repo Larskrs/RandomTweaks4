6b7528bb58f2dcfa953fb8fcb39f62d18d5efc4f
Larskrs <lkssyv@gmail.com>
2022-04-03 22:24:55 +0200

    Added Home Command

--- src/main/java/net/larskrs/plugins/randomtweaks4/command/HomeCommand.java
+++ src/main/java/net/larskrs/plugins/randomtweaks4/command/HomeCommand.java
@@ -1,9 +1,16 @@
 package net.larskrs.plugins.randomtweaks4.command;
 
+import net.larskrs.plugins.randomtweaks4.manager.ModuleManager;
+import net.larskrs.plugins.randomtweaks4.modules.DataFileManager;
 import net.larskrs.plugins.randomtweaks4.object.Command;
+import net.larskrs.plugins.randomtweaks4.tools.ConfigTools;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.List;
+import java.util.Objects;
 
 public class HomeCommand extends Command {
         public HomeCommand() {
@@ -17,7 +24,29 @@ public class HomeCommand extends Command {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
 
+            String homeName = (args.length == 1) ? args[0] : "Home";
+            if (!DataFileManager.getConfig().isConfigurationSection("player-data." + p.getUniqueId() + ".homes")) {
+                p.sendMessage(ChatColor.RED + " # You do not have any homes.");
+                return;
+            }
+            Boolean isHome = DataFileManager.getConfig().getConfigurationSection("player-data." + p.getUniqueId() + ".homes").getKeys(false).contains(homeName);
+
+                if (isHome) {
+                p.sendMessage(ChatColor.YELLOW + " > Teleported you to. " + ChatColor.RED + homeName);
+                Location HomeLocation = ConfigTools.getLocationFromConfSection(DataFileManager.getConfig(), "player-data." + p.getUniqueId() + ".homes." + homeName);
+                p.teleport(HomeLocation);
+                } else {
+                    p.sendMessage(ChatColor.RED + " # That is not a home.");
+                }
+
+
+
+        } else {
+            sender.sendMessage(ChatColor.RED + " # This command can only be ran by players.");
+        }
     }
 
     @Override
--- src/main/java/net/larskrs/plugins/randomtweaks4/command/SethomeCommand.java
+++ src/main/java/net/larskrs/plugins/randomtweaks4/command/SethomeCommand.java
@@ -28,14 +28,30 @@ public class SethomeCommand extends Command {
                 Player p = (Player) sender;
 
                 String homeName = (args.length == 1) ? args[0] : "Home";
+                Boolean hasHome = DataFileManager.getConfig().isConfigurationSection("player-data." + p.getUniqueId() + ".homes");
 
-                int playerHomes = DataFileManager.getConfig().getConfigurationSection("player-data." + p.getUniqueId() + ".homes.").getKeys(false).size();
-                if (ModuleManager.getModuleByName("HomeModule").getModuleFile().getConfig().getInt("maxHomes") > playerHomes) {
-                ConfigTools.setLocationToConfSection(DataFileManager.getConfig(), "player-data." + p.getUniqueId() + ".homes." + homeName, p.getLocation()); DataFileManager.saveFile(); DataFileManager.reloadFile();
-                p.sendMessage(ChatColor.YELLOW + " > Saved your home location. You can use " + ChatColor.RED + "/home " + ChatColor.YELLOW + "to come back to this place.");
-                } else {
-                    p.sendMessage(ChatColor.RED + " # You can't make more than " + playerHomes + " homes.");
-                }
+
+                    if (hasHome) {
+                        int playerHomes = DataFileManager.getConfig().getConfigurationSection("player-data." + p.getUniqueId() + ".homes.").getKeys(false).size();
+
+                        if (ModuleManager.getModuleByName("HomeModule").getModuleFile().getConfig().getInt("max-homes") > playerHomes) {
+                            ConfigTools.setLocationToConfSection(DataFileManager.getConfig(), "player-data." + p.getUniqueId() + ".homes." + homeName, p.getLocation());
+                            DataFileManager.saveFile();
+                            DataFileManager.reloadFile();
+                            p.sendMessage(ChatColor.YELLOW + " > Saved your home location. You can use " + ChatColor.RED + "/home " + ChatColor.YELLOW + "to come back to this place.");
+
+                        } else {
+                            p.sendMessage(ChatColor.RED + " # You can't make more than " + playerHomes + " homes.");
+                        }
+                    } else {
+                            ConfigTools.setLocationToConfSection(DataFileManager.getConfig(), "player-data." + p.getUniqueId() + ".homes." + homeName, p.getLocation());
+                            DataFileManager.saveFile();
+                            DataFileManager.reloadFile();
+                            p.sendMessage(ChatColor.YELLOW + " > Saved your home location. You can use " + ChatColor.RED + "/home " + ChatColor.YELLOW + "to come back to this place.");
+                    }
+
+
+
 
 
             } else {
